8d86cdd4a6d4b1c5c85d919c2cfa6bc398d2a901
74k4h1r0 <takahiro_22n1103806@nnn.ed.jp>
2024-06-14 19:20:31 +0900

    wall

--- brick-breaker/Assets/Scenes/CircleCon.cs
+++ brick-breaker/Assets/Scenes/CircleCon.cs
@@ -38,9 +38,11 @@ public class CircleCon : MonoBehaviour
 
     void OnTriggerEnter2D(Collider2D collider2D)
     {
+        Vector3 ballPos = transform.position;
+        Vector3 targetPos = collider2D.transform.position;
+
         string gameObjectTag = collider2D.gameObject.tag;
         Vector3 newVelocity = rb.velocity;
-        Debug.Log(newVelocity);
         if (gameObjectTag == "Item")
         {
             Destroy(collider2D.gameObject);
@@ -51,10 +53,18 @@ public class CircleCon : MonoBehaviour
         }
         else if (gameObjectTag == "Block")
         {
-            Debug.Log("Block");
-            Destroy(collider2D.gameObject);
-            newVelocity.y *= -1;
+            float directionX = (ballPos.x - targetPos.x);
+            Debug.Log(directionX);
 
+            if(directionX <= -0.4 ||directionX >= 0.4 )
+            {
+                newVelocity.x *= -1;
+            }
+            else
+            {
+                newVelocity.y *= -1; 
+            }
+            Destroy(collider2D.gameObject);
         }
         else if (gameObjectTag == "wall")
         {
@@ -68,7 +78,9 @@ public class CircleCon : MonoBehaviour
         }
         else if (gameObjectTag== "Player")
         {
-            newVelocity.y *= -1;
+            Vector3 direction = (ballPos - targetPos).normalized;
+            newVelocity = direction * speed;
+            newVelocity.x *= -1;
         }
 
         rb.velocity = newVelocity.normalized * speed;
